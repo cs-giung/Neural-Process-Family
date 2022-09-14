0b52f4958641f669ae78010e90d1eb1614697025
cs-giung <giung@kaist.ac.kr>
2022-09-14 17:32:20 +0000

    Update experiments (train_SearchBaselines)

---
+++ scripts_history/0914b_periodic250.sh
@@ -0,0 +1,23 @@
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/cnp.yaml  -lr 0.000500000000000
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/cnp.yaml  -lr 0.000889139705019
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/cnp.yaml  -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/cnp.yaml  -lr 0.002811706625952
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/cnp.yaml  -lr 0.005000000000000
+
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/canp.yaml -lr 0.000500000000000
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/canp.yaml -lr 0.000889139705019
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/canp.yaml -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/canp.yaml -lr 0.002811706625952
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/canp.yaml -lr 0.005000000000000
+
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/np.yaml   -lr 0.000500000000000
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/np.yaml   -lr 0.000889139705019
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/np.yaml   -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/np.yaml   -lr 0.002811706625952
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/np.yaml   -lr 0.005000000000000
+
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/anp.yaml  -lr 0.000500000000000
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/anp.yaml  -lr 0.000889139705019
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/anp.yaml  -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/anp.yaml  -lr 0.002811706625952
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/250/anp.yaml  -lr 0.005000000000000
---
+++ scripts_history/0914e_periodicinf.sh
@@ -0,0 +1,23 @@
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/cnp.yaml  -lr 0.000500000000000
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/cnp.yaml  -lr 0.000889139705019
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/cnp.yaml  -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/cnp.yaml  -lr 0.002811706625952
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/cnp.yaml  -lr 0.005000000000000
+
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/canp.yaml -lr 0.000500000000000
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/canp.yaml -lr 0.000889139705019
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/canp.yaml -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/canp.yaml -lr 0.002811706625952
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/canp.yaml -lr 0.005000000000000
+
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/np.yaml   -lr 0.000500000000000
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/np.yaml   -lr 0.000889139705019
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/np.yaml   -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/np.yaml   -lr 0.002811706625952
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/np.yaml   -lr 0.005000000000000
+
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/anp.yaml  -lr 0.000500000000000
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/anp.yaml  -lr 0.000889139705019
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/anp.yaml  -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/anp.yaml  -lr 0.002811706625952
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/periodic/inf/anp.yaml  -lr 0.005000000000000
