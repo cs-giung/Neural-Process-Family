e7228eb9613aabaa04bb699ca2db60de4003980a
cs-giung <giung@kaist.ac.kr>
2022-09-15 19:50:43 +0000

    Update experiments (train_SearchBaselines_image)

---
+++ scripts_history/0916e_celeba.sh
@@ -0,0 +1,6 @@
+# python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/celeba/banp.yaml  -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/celeba/canp.yaml  -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/celeba/anp.yaml   -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/celeba/bnp.yaml   -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/celeba/cnp.yaml   -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/celeba/np.yaml    -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
