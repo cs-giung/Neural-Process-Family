d619a35d4325c32f6b2c2f3755ab96b89d85bc58
cs-giung <giung@kaist.ac.kr>
2022-09-15 10:43:11 +0000

    Update experiments (train_SearchBaselines)

--- scripts_history/0915c_rbf.sh
+++ scripts_history/0915c_rbf.sh
@@ -1,8 +1,2 @@
-python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/rbf/inf/np.yaml   -lr 0.001581138830084 --model.kwargs.loss_type ml
-python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/rbf/inf/anp.yaml  -lr 0.001581138830084 --model.kwargs.loss_type ml
-
-python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/rbf/inf/np.yaml   -lr 0.001581138830084 --model.kwargs.loss_type iwae
-python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/rbf/inf/anp.yaml  -lr 0.001581138830084 --model.kwargs.loss_type iwae
-
-python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/rbf/inf/np.yaml   -lr 0.001581138830084 --model.kwargs.loss_type elbo
-python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/rbf/inf/anp.yaml  -lr 0.001581138830084 --model.kwargs.loss_type elbo
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/rbf/inf/bnp.yaml  -lr 0.001581138830084
+python scripts/jax/train_SearchBaselines.py -s 3 -f configs/gp/rbf/inf/banp.yaml -lr 0.001581138830084
