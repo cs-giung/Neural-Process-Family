cff06638e8c3791724d857e42e9ba6f9615e5e01
cs-giung <giung@kaist.ac.kr>
2022-09-16 14:16:06 +0000

    Update experiments (train_SearchBaselines_image)

---
+++ scripts_history/0916d_mnist.sh
@@ -0,0 +1,6 @@
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/mnist/banp.yaml  -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/mnist/canp.yaml  -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/mnist/anp.yaml   -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/mnist/bnp.yaml   -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/mnist/cnp.yaml   -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/mnist/np.yaml    -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
