a07b878ebd8084826181ac2fdfdecbf06dfeb438
cs-giung <giung@kaist.ac.kr>
2022-09-16 04:36:31 +0000

    Update experiments (train_SearchBaselines_image)

---
+++ scripts_history/0916c_svhn.sh
@@ -0,0 +1,6 @@
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/svhn/banp.yaml  -lr 0.000889139705019 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/svhn/canp.yaml  -lr 0.000889139705019 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/svhn/anp.yaml   -lr 0.000889139705019 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/svhn/bnp.yaml   -lr 0.000889139705019 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/svhn/cnp.yaml   -lr 0.000889139705019 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train_SearchBaselines_image.py -s 3 -f configs/image/svhn/np.yaml    -lr 0.000889139705019 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
