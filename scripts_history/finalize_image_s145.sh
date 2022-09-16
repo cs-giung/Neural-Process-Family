4207425fa4d3d6087cad2d78ed93bab39eb57e16
cs-giung <giung@kaist.ac.kr>
2022-09-16 08:01:35 +0000

    Update experiments (train)

---
+++ scripts_history/finalize_image_s145.sh
@@ -0,0 +1,15 @@
+# celeba
+python scripts/jax/train.py -s 145 -f configs/image/celeba/cnp.yaml  -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train.py -s 145 -f configs/image/celeba/np.yaml   -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
+python scripts/jax/train.py -s 145 -f configs/image/celeba/bnp.yaml  -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train.py -s 145 -f configs/image/celeba/canp.yaml -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train.py -s 145 -f configs/image/celeba/anp.yaml  -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
+python scripts/jax/train.py -s 145 -f configs/image/celeba/banp.yaml -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10
+
+# svhn
+python scripts/jax/train.py -s 145 -f configs/image/svhn/cnp.yaml    -lr 0.005000000000000 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train.py -s 145 -f configs/image/svhn/np.yaml     -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
+python scripts/jax/train.py -s 145 -f configs/image/svhn/bnp.yaml    -lr 0.005000000000000 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train.py -s 145 -f configs/image/svhn/canp.yaml   -lr 0.001581138830084 --datasets.train.batch_size 128 --train.num_epochs 10
+python scripts/jax/train.py -s 145 -f configs/image/svhn/anp.yaml    -lr 0.000889139705019 --datasets.train.batch_size 128 --train.num_epochs 10 --model.kwargs.loss_type ml
+python scripts/jax/train.py -s 145 -f configs/image/svhn/banp.yaml   -lr 0.002811706625952 --datasets.train.batch_size 128 --train.num_epochs 10
